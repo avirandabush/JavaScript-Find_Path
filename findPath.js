8088fb280a0a14ec2706d53f207ddd6e6a973122
aviran <avirandabush@gmail.com>
2022-05-10 16:01:51 +0300

    Create findPath.js

---
+++ findPath.js
@@ -0,0 +1,25 @@
+function findPath(arr)
+{
+    let row = arr.length-1;
+    let col = arr[0].length-1;
+    let r = 0;
+    let c = 0;
+    let start = arr[0][0];
+
+    return rec(arr, row, col, r, c, start);
+}
+
+function rec(arr, row, col, r, c, start)
+{
+    if(arr[r][c] == start)
+    {
+        if(r == row && c == col)
+            return true;
+        if(r == row && c < col)
+            return rec(arr, row, col, r, c+1, start);
+        if(r < row && c == col)
+            return rec(arr, row, col, r+1, c, start);
+        return(rec(arr, row, col, r+1, c, start) || rec(arr, row, col, r, c+1, start) || rec(arr, row, col, r+1, c+1, start));
+    }
+    return false;
+}
